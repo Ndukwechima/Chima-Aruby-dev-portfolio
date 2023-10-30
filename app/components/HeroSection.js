abae75fbd78b733c7765213108e8f83bc9b1b955
Ndukwe Chima <chimandukwe.r@gmail.com>
2023-10-30 11:50:54 +0100

    fixed some html entity issues

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -11,7 +11,7 @@ function HeroSection() {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#673be6] to-[#00c0cf]">
-              Hello, I'm
+              Hello, I&apos;m
             </span>
             <br></br>
             <TypeAnimation
